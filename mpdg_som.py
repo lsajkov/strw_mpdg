22014b6017fcbdafc9d2f0c2412d34a19bd01480
lsajkov <leonid.sajkov@gmail.com>
2024-06-28 14:04:11 +0200

    built labeling function. made first predictions

--- mpdg_som.py
+++ mpdg_som.py
@@ -4,6 +4,14 @@ from numpy import linalg as linalg
 from mpdg_som_utils import chi_sq_dist
 from mpdg_som_utils import training_step
 
+import matplotlib.pyplot as plt
+plt.rcParams.update({
+    'font.family': 'serif',
+    'font.size': 24,
+    'axes.labelsize': 'large',
+    'mathtext.fontset': 'stix'
+})
+
 from mpdg_som_utils import SOM_LearningRateFunctions as lrf
 from mpdg_som_utils import SOM_NeighborhoodFunctions as nhb
 from mpdg_som_utils import SOM_ErrorEstimators as e_est
@@ -136,6 +144,9 @@ class SelfOrganizingMap:
         SOM_space = self.mapsize.copy()
         SOM_space.append(self.data_dim)
 
+        if self.map_dimensionality > self.data_dim:
+            raise(AssertionError('The map cannot have more dimensions than the data.'))
+
         if self.initialization == 'random':
             random_idx = np.random.rand(*self.mapsize) * self.data_len
             random_idx = np.array(random_idx, dtype = int)
@@ -174,7 +185,7 @@ class SelfOrganizingMap:
         complete_variance = self.variances.copy()
 
         error = 99
-        while error > error_thresh:
+        while self.step < self.maximum_steps:
             for index in range(self.data_len):
                 weights, bmu_coords = training_step(weights,
                                                     complete_data[index],
@@ -210,6 +221,81 @@ class SelfOrganizingMap:
         #         self.bmu_indices[index] = (bmu_coords)
         #     self.step += 1
 
+    def label_map(self,
+                  parameters,
+                  parameter_names = None):
+        
+        if len(np.shape(parameters)) == 2:
+            self.parameters = np.array(parameters)
 
-    #next: build error estimator
-    #after: build method for labelling pixels
+        elif (len(np.shape(parameters)) == 1) & (len(parameters)> 1):
+
+            tuple_params = parameters.as_array()
+            list_params = [list(values) for values in tuple_params]
+            self.parameters = np.array(list_params)
+
+        if np.shape(self.parameters)[0] != self.data_len:
+            raise(AssertionError('The number of parameter data points does not match the number of data points used to build the map!'))
+
+        self.params_dim = np.shape(self.parameters)[1]
+
+        if parameter_names != None:
+            self.parameter_names = parameter_names
+        
+        elif parameter_names == None:
+            self.parameter_names = [f'param{i}' for i in range(self.params_dim)]
+
+        populated_cells = np.unique(self.bmu_indices, axis = 0)
+
+        if len(populated_cells) > np.prod(self.mapsize):
+            raise(AssertionError('There are more populated cells than there are cells in the entire map. Check logic.'))
+
+        self.map_labels = np.full([*self.mapsize, self.params_dim], np.nan)
+
+        for cell in populated_cells:
+
+            matching_idx = np.all(self.bmu_indices == cell, axis = -1)
+            self.map_labels[*cell] = np.median(self.parameters[matching_idx], axis = 0)
+
+    def show_map(self, show_labeled = False,
+                 cmap = 'jet_r'):
+
+        if self.map_dimensionality != 2:
+            raise(NotImplementedError('The module can only display 2-d maps for now.'))
+
+        print(f'\n| SOM. Step {self.step}. Initialization: {self.initialization}')
+
+
+        if not show_labeled:
+
+            fig = plt.figure(figsize = (self.data_dim * 5, 10), constrained_layout = True)
+            
+            for i, name in enumerate(self.variable_names):
+                ax = fig.add_subplot(1, self.data_dim, i + 1)
+                imsh = ax.imshow(self.weights_map[..., i], origin = 'lower', cmap = cmap)
+                ax.axis('off')
+                fig.colorbar(mappable = imsh, ax = ax,
+                            label = name, location = 'bottom', pad = 0.01)
+
+        if show_labeled:
+
+            variables_dim = np.shape(self.variable_names)[1]
+            fig = plt.figure(figsize = (self.variables_dim * 5, 10), constrained_layout = True)
+            
+            for i, name in enumerate(self.parameter_names):
+                ax = fig.add_subplot(1, variables_dim, i + 1)
+                imsh = ax.imshow(self.map_labels[..., i], origin = 'lower', cmap = cmap)
+                ax.axis('off')
+                fig.colorbar(mappable = imsh, ax = ax,
+                             label = name, location = 'bottom', pad = 0.01)
+                
+    def predict(self,
+                prediction_input):
+        
+        raise(NotImplementedError)
+                
+    def prediction_statistics(self,
+                              return_values = False):
+        
+                        
+        raise(NotImplementedError)
